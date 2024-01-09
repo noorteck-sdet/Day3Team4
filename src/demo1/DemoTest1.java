a9baf79a5bf797348baf0f25f3cb20dc89ac7cc3
alaza <alaza@Ahmed>
2024-01-08 21:23:34 -0500

    pull req

--- src/demo1/DemoTest1.java
+++ src/demo1/DemoTest1.java
@@ -17,7 +17,8 @@ public static void main(String[] args) {
 	// hello from Gio
 	
 	//Hi from Sinan
-	// hi from Ezra
+	
+	//*******************************pull req*****************************
 }
 	
 	
