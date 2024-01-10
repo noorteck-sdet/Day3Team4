4036b5c79bf9f987a1c4885e896e1e149c20404d
JAZUMA <JAZUMA@10.0.0.3>
2024-01-09 18:38:12 -0800

    initial commit

---
+++ Demo4.java
@@ -0,0 +1,32 @@
+package team4;
+
+public class Demo4 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Team4 .. YAAY!");
+	}
+// sam
+	// test1223
+
+	
+//	//method by Ivan
+//	public static void printInteger(int i) {
+//		
+//		Integer[] d = null;
+//		for (Integer integer : d) {
+//
+//			System.out.println(integer);
+//
+//		}
+//		
+//	}
+	//From Cristian
+	public static int method1(int a, int b) {
+		int result = 0;
+		result = a + b;
+		return result;
+	}
+}
+
+// Ivan 3
